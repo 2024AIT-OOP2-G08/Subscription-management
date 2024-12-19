2650a5d1b226554250bb89c8306f980bb81680cb
harry46497 <k23119kk@aitech.ac.jp>
2024-12-20 00:56:32 +0900

    データベースに各値を登録する機能を追加

--- routes/add.py
+++ routes/add.py
@@ -1,19 +1,22 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from models import Contents
+from datetime import date
 
 add_bp = Blueprint('add', __name__, url_prefix='/add')
 
 #データ追加ページの処理を追加
-#はやて
-
-
 @add_bp.route('/', methods=['GET', 'POST'])
 def add():
-
     #POSTメソッドでリクエストがあった場合の処理
-    # ヒント if request.method == 'POST':
+    if request.method == 'POST':
+        created_at = date.today()
+        name = request.form['name']
+        price = int(request.form['price'])
+        entry = date.fromisoformat(request.form['entry'])
+        payment_date = date.fromisoformat(request.form['payment_date'])
+        Contents.create(created_at=created_at, name=name, price=price, entry=entry, payment_date=payment_date)
 
+        return redirect(url_for('index'))
 
-    
     #入力するだけなのでテンプレートに送らない
     return render_template('add.html')
