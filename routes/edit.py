ca3e628075dc3b525d6510b6ebf5ac1a09ea028d
ayato1027 <k22235kk@aitech.ac.jp>
2024-12-25 19:53:56 +0900

    edit first commit

--- routes/edit.py
+++ routes/edit.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from models import Contents
+from datetime import date
 
 edit_bp = Blueprint('edit', __name__, url_prefix='/edit')
 
-@edit_bp.route('/')
-def edit():
-    return render_template('edit.html')
+@edit_bp.route('edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    content = Contents.get(Contents.id == id)
+    if not content:
+        return redirect(url_for('index'))
+    #POSTメソッドでリクエストがあった場合の処理
+    if request.method == 'POST':
+        content.name = request.form['name']
+        content.price = int(request.form['price'])
+        content.entry = date.fromisoformat(request.form['entry'])
+        content.payment_date = date.fromisoformat(request.form['payment_date'])
+        content.save()
+        return redirect(url_for('index'))
+
+    #編集するデータをテンプレートに送る
+    return render_template('edit.html', content=content)
