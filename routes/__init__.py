826d8c86db15da2cc938986ac924cd7e73cb5880
ayato1027 <k22235kk@aitech.ac.jp>
2024-12-28 19:04:05 +0900

    delete_bpの追加

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,6 @@
 from .content import content_bp
 from .add import add_bp
 from .edit import edit_bp
+from .delete import delete_bp
 
-blueprints = [content_bp, add_bp, edit_bp]
+blueprints = [content_bp, add_bp, edit_bp, delete_bp]
